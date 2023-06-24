252f533339c2e1f669ec83973a9a6587619c2f04
serosc95 <oscardleon95@gmail.com>
2023-06-23 21:19:26 -0500

    add controller client

---
+++ net-core/Store/Infraestructura/Configs/ClienteConfig.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Dominio;
+
+namespace Store.Infraestructura.Configs {
+	class ClienteConfig : IEntityTypeConfiguration<Cliente> {
+		public void Configure(EntityTypeBuilder<Cliente> builder) {
+			builder.ToTable("Cliente");
+			builder.HasKey(c => c.clienteId);
+		}
+	}
+}
---
+++ net-core/Store/Infraestructura/Controllers/ClienteController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Dominio;
+using Store.Aplicaciones.Servicios;
+using Store.Infraestructura.Contextos;
+using Store.Infraestructura.Repositorios;
+
+namespace Store.Infraestructura.Controllers {
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ClienteController : ControllerBase {
+
+		ClienteServicio CrearServicio() {
+			VentaContexto db = new VentaContexto();
+			ClienteRepositorio repo = new ClienteRepositorio(db);
+			ClienteServicio servicio = new ClienteServicio(repo);
+			return servicio;
+		}
+		
+		//GET api/cliente
+		[HttpGet]
+		public ActionResult<List<Cliente>> Get() {
+			var servicio = CrearServicio();
+			return Ok(servicio.Listar());
+		}
+
+		//GET api/cliente/{id}
+		[HttpGet("{id}")]
+		public ActionResult<Cliente> Get(Guid id) {
+			var servicio = CrearServicio();
+			return Ok(servicio.SeleccionarPorID(id));
+			
+		}
+
+		//POST api/cliente
+		[HttpPost]
+		public ActionResult Post([FromBody] Cliente cliente) {
+			var servicio = CrearServicio();
+			servicio.Agregar(cliente);
+			return Ok("Se creo el cliente correctamente");
+		}
+
+		//PUT api/cliente/{id}
+		[HttpPut("{id}")]
+		public ActionResult Put(Guid id, [FromBody] Cliente cliente) {
+			var servicio = CrearServicio();
+			cliente.clienteId = id;
+			servicio.Editar(cliente);
+			return Ok(string.Format("Se edito el cliente {0:d} correctamente", id));
+		}
+
+		//DELETE api/cliente/{id}
+		[HttpDelete("{id}")]
+		public ActionResult Delete(Guid id) {
+			var servicio = CrearServicio();
+			servicio.Eliminar(id);
+			return Ok(string.Format("Se elimino el cliente {0:d} correctamente", id));
+		}
+	}
+}
